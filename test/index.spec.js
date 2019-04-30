69cddeb69dfc43661f2aec339084b8bdca515256
Carina Reketis <carina.reketis@gmail.com>
2019-04-30 16:19:12 -0300

    implementação do teste que verifica campo vazio e função

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,3 +2,10 @@ const chai = require ("chai");
 const expect = chai.expect;
 const Index = require("../lib/index");
 
+describe("index lib", () => {
+    describe("cpfChecksInput", () => {
+        it('cpf can not be empty', () => {
+            expect(Index.cpfChecksInput('31832055809')).to.equal(true);
+        });
+});
+});
