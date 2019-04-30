7527c5f8540255d15990dd15722f21504cceb8b2
Carina Reketis <carina.reketis@gmail.com>
2019-04-30 18:15:35 -0300

    teste e função cpfChecksInput funcionando

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,7 +5,16 @@ const Index = require("../lib/index");
 describe("index lib", () => {
     describe("cpfChecksInput", () => {
         it('cpf can not be empty', () => {
-            expect(Index.cpfChecksInput('31832055809')).to.equal(true);
+            expect(Index.cpfChecksInput('')).to.equal(false);
         });
-});
+        it('cpf characters less than < 11', () => {
+            expect(Index.cpfChecksInput('12345')).to.equal(false);
+        });
+        it('cpf characters greater than > 11', () => {
+            expect(Index.cpfChecksInput('123456789101112')).to.equal(false);
+        });
+        it('cpf characters equal', () => {
+            expect(Index.cpfChecksInput('11111111111')).to.equal(false);
+        });
+    });
 });
