95a23a623d4e56f807bf1676ef6b419e26e4403f
Carina Reketis <carina.reketis@gmail.com>
2019-05-01 17:02:35 -0300

    refatorei o código, falta desmembrar em funções menores e corrigir 2 erros linha 14 e 25 index.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,20 +1,22 @@
-const chai = require ("chai");
+const chai = require('chai');
 const expect = chai.expect;
-const Index = require("../lib/index");
-
-describe("index lib", () => {
-    describe("cpfChecksInput", () => {
-        it('cpf can not be empty', () => {
-            expect(Index.cpfChecksInput('')).to.equal(false);
-        });
-        it('cpf characters less than < 11', () => {
-            expect(Index.cpfChecksInput('12345')).to.equal(false);
-        });
-        it('cpf characters greater than > 11', () => {
-            expect(Index.cpfChecksInput('123456789101112')).to.equal(false);
-        });
-        it('cpf characters equal', () => {
-            expect(Index.cpfChecksInput('11111111111')).to.equal(false);
-        });
+const Index = require('../lib/index');
+describe('index lib', () => {
+  describe('cpfValidator', () => {
+    it('cpf does not accept string', () => {
+      expect(Index.cpfValidator('1a2b3c4d5e6')).to.equal(false);
     });
+    it('cpf can not be empty', () => {
+      expect(Index.cpfValidator('')).to.equal(false);
+    });
+    it('cpf characters less than < 11', () => {
+      expect(Index.cpfValidator('12345')).to.equal(false);
+    });
+    it('cpf characters greater than > 11', () => {
+      expect(Index.cpfValidator('123456789101112')).to.equal(false);
+    });
+    it('cpf characters equal', () => {
+      expect(Index.cpfValidator('11111111111')).to.equal(false);
+    });
+  });
 });
