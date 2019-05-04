744c8488451d52ebe368b84e75e726b3e77116d7
Carina Reketis <carina.reketis@gmail.com>
2019-05-04 14:07:48 -0300

    código refatorado

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,10 @@
-const chai = require('chai');
+const chai = require ("chai");
 const expect = chai.expect;
-const Index = require('../lib/index');
+const Index = require("../lib/index");
 describe('index lib', () => {
   describe('cpfValidator', () => {
     it('cpf does not accept string', () => {
-      expect(Index.cpfValidator('1a2b3c4d5e6')).to.equal(false);
+      expect(Index.cpfValidator('ascderfgthy')).to.equal(false);
     });
     it('cpf can not be empty', () => {
       expect(Index.cpfValidator('')).to.equal(false);
@@ -16,7 +16,13 @@ describe('index lib', () => {
       expect(Index.cpfValidator('123456789101112')).to.equal(false);
     });
     it('cpf characters equal', () => {
-      expect(Index.cpfValidator('11111111111')).to.equal(false);
+      expect(Index.cpfValidator('00000000000')).to.equal(false);
+    });
+    it('cpf characters completed', () => {
+      expect(Index.cpfValidator('31832055809')).to.equal(true);
+    });
+    it('cpf invalid', () => {
+      expect(Index.cpfValidator('31832055890')).to.equal(false);
     });
   });
 });
